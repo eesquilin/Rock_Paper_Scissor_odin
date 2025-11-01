c4fcd45f26b8238b281342634f4bb459b23d604e
eesquilinv <emmanuel.esquilinvargas@gmail.com>
2025-11-01 10:43:40 -0400

    refactor getHumanChoice and playRound functions for improved structure and clarity

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function getCOmputerChoice() {
 }
 
 function getHumanChoice() {
-   
+
     const choice = prompt("Your choice: Paper(0) Rock(1) Scissor(2). ");
     if (choice === "0") {
         console.log("Paper");
@@ -19,7 +19,30 @@ function getHumanChoice() {
     } else if (choice === "2") {
         console.log("Scissor");
         return "Scissor"
-    } 
+    }
+}
+
+//Variables for score 
+humanScore = 0;
+ComputerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = getHumanChoice().toLowerCase(); 
+    computerChoice = getCOmputerChoice().toLowerCase();
+
+
+    if (humanChoice === computerChoice) {
+        console.log("Round is a tie.");
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        console.log("Human wins the round.");
+    } else {
+        console.log("Computer wins the round.");
+    }
+
 }
 
 
